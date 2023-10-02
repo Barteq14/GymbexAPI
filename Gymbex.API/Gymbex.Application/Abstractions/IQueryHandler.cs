9d0f8f1a96c032ac1e6329c65ccdae8a7be1d8ad
Bartłomiej Longota <barti1222@gmail.com>
2023-10-02 11:58:36 +0200

    Added application layer with one query (GetAcitvities)

---
+++ Gymbex.API/Gymbex.Application/Abstractions/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Application.Abstractions
+{
+    public interface IQueryHandler<in TQuery, TResult> where TQuery : class, IQuery<TResult>
+    {
+        Task<TResult> ExecuteHandleAsync(TQuery query);
+    }
+}
