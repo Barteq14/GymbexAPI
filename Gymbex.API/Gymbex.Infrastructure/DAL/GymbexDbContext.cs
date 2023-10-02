df7be839ca94dff27d01d3ec7534b57bafb631a5
Bartłomiej Longota <barti1222@gmail.com>
2023-10-02 11:25:52 +0200

    Added Infrastructure layer

---
+++ Gymbex.API/Gymbex.Infrastructure/DAL/GymbexDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gymbex.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gymbex.Infrastructure.DAL
+{
+    public sealed class GymbexDbContext : DbContext
+    {
+        public GymbexDbContext(DbContextOptions<GymbexDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+
+
+    }
+}
