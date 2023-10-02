db7ef3465994d3903aa0982abc4276f68efbc919
Bartłomiej Longota <barti1222@gmail.com>
2023-10-02 10:49:36 +0200

    Added basic enitites

---
+++ Gymbex.API/Gymbex.Core/Entities/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Core.Entities
+{
+    public sealed class Customer
+    {
+        /// <summary>
+        /// Id użytkownika
+        /// </summary>
+        public Guid Id { get; private set; }
+        /// <summary>
+        /// Nazwa użytkownika
+        /// </summary>
+        public string Username { get; private set; }
+        /// <summary>
+        /// Hasło użytkownika
+        /// </summary>
+        public string Password { get; private set; }
+        /// <summary>
+        /// Imię i nazwisko użytkownika
+        /// </summary>
+        public string Fullname { get; private set; }
+        /// <summary>
+        /// Email użytkownika
+        /// </summary>
+        public string Email { get; private set; }
+        /// <summary>
+        /// Numer telefonu
+        /// </summary>
+        public string PhoneNumber { get; private set; }
+
+        #region Relationships
+        public Guid TicketId { get; private set; }
+        public Ticket Ticket { get; private set; }
+
+        #endregion
+
+
+        public Customer(Guid id, string username, string password, string fullname, string email, string phoneNumber, Guid ticketId, Ticket ticket)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            TicketId = ticketId;
+            Ticket = ticket;
+        }
+    }
+}
---
+++ Gymbex.API/Gymbex.Core/Enums/TicketKindEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Core.Enums
+{
+    public enum TicketKindEnum
+    {
+        [Display(Name = "Karnet na jeden dzień")]
+        OneDay = 0,
+        [Display(Name = "Karnet na miesiąc")]
+        Month = 1,
+        [Display(Name = "Karnet na pół roku")]
+        HalfYear = 2,
+        [Display(Name = "Karnet na rok")]
+        Year = 3,
+    }
+}
