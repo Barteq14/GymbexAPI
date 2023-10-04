6ca4526a0ec4de164add51087467b533708fdd62
Bartłomiej Longota <barti1222@gmail.com>
2023-10-04 13:50:40 +0200

    Added abstract for hash & verify user password

---
+++ Gymbex.API/Gymbex.Infrastructure/Secure/PasswordManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gymbex.Application.Security;
+using Gymbex.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gymbex.Infrastructure.Secure
+{
+    internal sealed class PasswordManager : IPasswordManager
+    {
+        private readonly IPasswordHasher<Customer> _passwordHasher;
+
+        public PasswordManager(IPasswordHasher<Customer> passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public string Secure(string password)
+            => _passwordHasher.HashPassword(default, password);
+
+        public bool Validate(string password, string securedPassword)
+            => _passwordHasher.VerifyHashedPassword(default, securedPassword, password)
+                is PasswordVerificationResult.Success;
+    }
+}
