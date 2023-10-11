e859f16edc6815fd3d506a16f7d4959e233382d3
Bartłomiej Longota <barti1222@gmail.com>
2023-10-11 13:12:56 +0200

    Get first response (get all Activities)

---
+++ Gymbex.API/Gymbex.Blazor/Pages/Activities.razor.cs
@@ -0,0 +1,26 @@
+﻿using Gymbex.Blazor.Models;
+using System.Text.Json;
+
+namespace Gymbex.Blazor.Pages
+{
+    public partial class Activities
+    {
+        private string API_URL = "https://localhost:7291/";
+        private IEnumerable<ActivityDto> ActivitiesList = Enumerable.Empty<ActivityDto>();
+        private async Task RefreshActivitiesList()
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync($"{API_URL}api/Activity");
+            client.Dispose();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            ActivitiesList = await JsonSerializer.DeserializeAsync<IEnumerable<ActivityDto>>(responseStream);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshActivitiesList();
+        }
+    }
+}
