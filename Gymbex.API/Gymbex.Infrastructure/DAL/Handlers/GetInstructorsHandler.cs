0065d0d6c957d9900377a12ec5e1a3c2c49145da
Bartłomiej Longota <barti1222@gmail.com>
2023-10-26 09:56:33 +0200

    Get all instructors

---
+++ Gymbex.API/Gymbex.Infrastructure/DAL/Handlers/GetInstructorsHandler.cs
@@ -0,0 +1,33 @@
+﻿using Gymbex.Application.Abstractions;
+using Gymbex.Application.Dtos;
+using Gymbex.Application.Queries.Customers;
+using Gymbex.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Infrastructure.DAL.Handlers
+{
+    public sealed class GetInstructorsHandler : IQueryHandler<GetInstructors, List<CustomerDto>>
+    {
+        private readonly GymbexDbContext _dbContext;
+
+        public GetInstructorsHandler(GymbexDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<CustomerDto>> ExecuteHandleAsync(GetInstructors query)
+        {
+            var instructorRole = Role.Instructor();
+
+            var instructors =  await _dbContext.Customers
+                .Where(x => x.Role == instructorRole).ToListAsync();
+
+            return instructors.Select(x => x.AsCustomerDto()).ToList();
+        }
+    }
+}
