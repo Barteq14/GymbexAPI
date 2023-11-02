2ce6c8539aec490a485fb01a6fd84b234c75d5bc
Bartłomiej Longota <barti1222@gmail.com>
2023-11-02 10:47:41 +0100

    Created entities for equipment & category

---
+++ Gymbex.API/Gymbex.Core/Entities/CategoryEquipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Core.Entities
+{
+    public sealed class CategoryEquipment
+    {
+        private static readonly HashSet<Equipment> _equipments = new();
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int TotalQuantity { get; set; }
+
+        public IEnumerable<Equipment> Equipments { get; private set; } = _equipments;
+
+        public CategoryEquipment(Guid id, string name, int totalQuantity)
+        {
+            Id = id;
+            Name = name;
+            TotalQuantity = totalQuantity;
+        }
+
+        public CategoryEquipment Create(Guid id, string name, int totalQuantity)
+            => new CategoryEquipment(Id, name, totalQuantity);
+
+        public void AddEquipment(Equipment equipment)
+        {
+            _equipments.Add(equipment);
+        }
+
+        public void RemoveEquipment(Equipment equipment)
+        {
+            _equipments.Remove(equipment);
+        }
+    }
+}
---
+++ Gymbex.API/Gymbex.Core/Entities/Equipment.cs
@@ -0,0 +1,35 @@
+﻿using Gymbex.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gymbex.Core.Entities
+{
+    public class Equipment
+    {
+        Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public EquipmentState EquipmentState { get; set; }
+        public int Quantity { get; set; }
+        public Guid CategoryEquipmentId { get; set; }
+        public CategoryEquipment CategoryEquipment { get; set; }
+
+
+        public Equipment(Guid id, string name, string description, EquipmentState equipmentState, int quantity, Guid categoryId)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            EquipmentState = equipmentState;
+            Quantity = quantity;
+            CategoryEquipmentId = categoryId;
+        }
+
+        public Equipment Create(Guid id, string name, string description, EquipmentState equipmentState, int quantity, Guid categoryId)
+                => new Equipment(id, name, description, equipmentState, quantity, categoryId);
+
+    }
+}
